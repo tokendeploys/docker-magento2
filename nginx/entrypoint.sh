76ff227c086f94e7d352bb193103bd76f8e54779
Steve Desmet <steve.desmet@gmail.com>
2017-06-25 19:44:30 +0400

    Use nginx.conf from root dir

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-wget -P /etc/nginx/conf.d/ https://raw.githubusercontent.com/magento/magento2/${MAGE_VERSION}/nginx.conf.sample
 envsubst '\$FCGI_HOST \$FCGI_PORT \$__MAGE_ROOT' < /etc/nginx/conf.d/default.conf.template \
     > /etc/nginx/conf.d/default.conf
 

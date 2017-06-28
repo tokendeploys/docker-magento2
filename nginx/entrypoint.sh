d9ee7609b515d625b78732b5af9aa6ba8d2fc2ab
Steve Desmet <steve.desmet@gmail.com>
2017-06-28 21:04:15 +0400

    Fix missing MAGE_MODE nginx variable

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-envsubst '\$FCGI_HOST \$FCGI_PORT \$__MAGE_ROOT' < /etc/nginx/conf.d/default.conf.template \
+envsubst '\$FCGI_HOST \$FCGI_PORT \$__MAGE_ROOT \$__MAGE_MODE' < /etc/nginx/conf.d/default.conf.template \
     > /etc/nginx/conf.d/default.conf
 
 exec "$@"

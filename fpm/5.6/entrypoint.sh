1e93c5dc1879cf2564c054dc8ea2492ff44bd36a
Steve Desmet <steve.desmet@gmail.com>
2017-06-29 20:27:42 +0400

    ONBUILD statements

--- fpm/5.6/entrypoint.sh
+++ fpm/5.6/entrypoint.sh
@@ -7,11 +7,15 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 # Create an owner user/group based on OWNER_* environment variables
-id -g magento &> /dev/null || addgroup -g $OWNER_GID magento
-id -u magento &> /dev/null || adduser -u $OWNER_UID -G magento -D -s /bin/sh magento
+id -g $OWNER_NAME &> /dev/null || addgroup -g $OWNER_GID $OWNER_NAME
+id -u $OWNER_NAME &> /dev/null || adduser -u $OWNER_UID -G $OWNER_NAME -D -s /bin/sh $OWNER_NAME
 
-# Update config to run the pool workers as this user
-sed -i 's/www-data/magento/g' /usr/local/etc/php-fpm.d/www.conf
+# Update config to run the pool workers as this user/group
+sed -ri "s/^user.*/user = $OWNER_NAME/g" /usr/local/etc/php-fpm.d/www.conf
+sed -ri "s/^group.*/group = $OWNER_NAME/g" /usr/local/etc/php-fpm.d/www.conf
+
+# Correct 
+find $MAGE_ROOT -user root -exec chown $OWNER_NAME:$OWNER_NAME {} \;
 
 {
     echo "xdebug.remote_enable = ${XDEBUG_ENABLE}"

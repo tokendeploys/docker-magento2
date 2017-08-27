2e375f4d56c821dc5ca7bca1af0e7957dbae35bd
Steve Desmet <steve.desmet@tokendeploys.com>
2017-08-27 15:06:14 +0200

    Set bash as shell

--- fpm/debian/7.0/scripts/tokendeploys-set-owner.sh
+++ fpm/debian/7.0/scripts/tokendeploys-set-owner.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Create an owner user/group based on OWNER_* environment variables
 id -g $3 &> /dev/null || groupadd -g $2 $3
-id -u $3 &> /dev/null || useradd -u $1 -g $1 -m -s /bin/sh $3
+id -u $3 &> /dev/null || useradd -u $1 -g $1 -m -s /bin/bash $3
 
 # Update config to run the pool workers as this user/group
 sed -ri "s/^user.*/user = $3/g" /usr/local/etc/php-fpm.d/www.conf

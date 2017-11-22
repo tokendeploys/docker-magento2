9019ad370697e839fb6544062e47684376bcb997
Steve Desmet <steve.desmet@tokendeploys.com>
2017-11-22 11:53:12 +0400

    mailhog

--- fpm/debian/7.0/scripts/tokendeploys-mailhog.sh
+++ fpm/debian/7.0/scripts/tokendeploys-mailhog.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Set ssmtp to use the mailhog smtp server on port 1025 by setting
+# its mailhub configuration setting
+# Assumes "mail" as DNS for mailhog container
 set -e
 
-echo "sendmail_path = /usr/sbin/sendmail -S mail:1025" > /usr/local/etc/php/conf.d/mailhog.ini
+sed -ri 's/^mailhub.*/mailhub=mail:1025/g' /etc/ssmtp/ssmtp.conf
